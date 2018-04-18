7bf7645802ce363a939a2a7c6891cbf510285eaa
Evan Dorsky <evan.dorsky@icloud.com>
2018-04-17 18:23:56 -0700

    Add Caps Lock -> Ctrl (for MacBook Pr

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-keyboard_id="$(ioreg -n 'Apple Internal Keyboard / Trackpad' -r | grep -e idProduct\" -e idVendor | tr -d \"\|[:blank:] | cut -d\= -f2 | tr '\n' -)"
-
+keyboard_id=$(ioreg -n 'Apple Internal Keyboard / Trackpad' -r | grep -e idVendor -e idProduct | tail -r | sed 's/.*= //' | tr '\n' -)
 defaults -currentHost write -g com.apple.keyboard.modifiermapping."$keyboard_id"0 -array '<dict><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer><key>HIDKeyboardModifierMappingDst</key><integer>2</integer></dict>'
