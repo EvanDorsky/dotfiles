7d62f8589249b34779528ef8dd2df705f13c7ef7
Evan Dorsky <evan.dorsky@icloud.com>
2018-04-18 09:37:36 -0700

    More init scripts

---
+++ scripts/brew.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+
+# Install other useful binaries.
+brew install imagemagick --with-webp
+brew install rename
+brew install tree
+
+# And more
+brew install ag
+brew install htop
+brew install ranger
+brew install autojump
+
+# System Python is hell
+brew install python
+brew install python@2
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ scripts/vim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir ~/.vim
+mkdir ~/.vim/backups
+mkdir ~/.vim/swaps
+mkdir ~/.vim/undo
