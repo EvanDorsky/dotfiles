e4cd1bc8da5925914c8d7dd81a668705b61c7fe7
Evan Dorsky <evan.dorsky@icloud.com>
2020-04-02 09:00:58 -0700

    Add "headless" option for init

--- init.sh
+++ init.sh
@@ -2,16 +2,24 @@
 
 # Enable TouchID for sudo (comment out if Mac doesn't have TouchID!)
 # If this goes wrong, can only be fixed in single user mode (since it breaks sudo)
-sudo (echo "auth sufficient pam_tid.so\n"; cat /etc/pam.d/sudo) > /etc/pam.d/sudo
+# sudo (echo "auth sufficient pam_tid.so\n"; cat /etc/pam.d/sudo) > /etc/pam.d/sudo
 
+if [ "$1" = "headless" ]; then
+	headless=true
+fi;
+
+# if [ -n "$headless" ]; then
+# if [ -z "$headless" ]; then
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install useful homebrew packages
-./scripts/brew.sh
+if [ -z "$headless" ]; then
+	# Install useful homebrew packages
+	./scripts/brew.sh
 
-# Install cask apps
-./scripts/cask.sh
+	# Install cask apps
+	./scripts/cask.sh
+fi;
 
 # Set System defaults
 export DOTFILES_ROOT=".dotfiles"
@@ -27,15 +35,16 @@ export DOTFILES_ROOT=".dotfiles"
 # 	...
 # ...
 
-# Sublime
-cp apps/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+if [ -z "$headless" ]; then
+	# Sublime
+	cp apps/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
+	# Set login items
+	./scripts/loginitems.sh
 
-# Set login items
-./scripts/loginitems.sh
-
-# Fix keyboard
-./scripts/keyboard.sh
+	# Fix keyboard
+	./scripts/keyboard.sh
+fi;
 
 # Create ssh key and add to GitHub account
 ./scripts/ssh.sh
@@ -43,8 +52,9 @@ cp apps/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Subl
 # Configure git
 git config --global user.name "Evan Dorsky"
 git config --global user.email evan.dorsky@icloud.com
-git config --global alias.l "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-# Set macOS defaults
-./.macos
-
+if [ -z "$headless" ]; then
+	# Set macOS defaults
+	./.macos
+fi;
